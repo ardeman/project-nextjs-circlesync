094970a1662244ccb851f1107a543276cd44f916
Ardeman <are.theman@gmail.com>
2024-10-10 16:58:42 +0800

    Refactor dashboard page component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,8 @@
-import { useFirebaseAuth } from '@/hooks'
-
 export default function Dashboard() {
-  const { user } = useFirebaseAuth()
-
   return (
     <div>
       <h1>Dashboard</h1>
-      <p>Welcome, {user?.email}</p>
+      <p>Welcome</p>
     </div>
   )
 }
