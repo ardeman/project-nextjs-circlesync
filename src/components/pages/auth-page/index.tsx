3b9f70a16bda5f1b465a5bb5a0e6e946f9a6da04
Ardeman <are.theman@gmail.com>
2024-10-14 13:31:25 +0800

    Add favicon and site manifest files
    This commit adds the favicon and site manifest files to the project. The favicon was generated using graphics from Twitter Twemoji, and the site manifest file defines the icons and theme colors for the web app.

--- src/components/pages/auth-page/index.tsx
+++ src/components/pages/auth-page/index.tsx
@@ -35,7 +35,7 @@ export const AuthPage: FC = () => {
               className="space-y-6"
             >
               <h2 className="text-center text-2xl font-bold text-gray-800 dark:text-gray-200">
-                Welcome
+                Circle Sync
               </h2>
 
               <Input
