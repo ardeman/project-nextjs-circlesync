8172b8400447306139930fc03c0c886d4b124393
Ardeman <are.theman@gmail.com>
2024-10-16 19:14:51 +0800

    Refactor component imports and update layout in dashboard

---
+++ src/app/dashboard/settings/layout.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import Link from 'next/link'
+import { ReactNode } from 'react'
+
+const SettingsLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="bg-muted/40 flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
+      <div className="mx-auto grid w-full max-w-6xl gap-2">
+        <h1 className="text-3xl font-semibold">Settings</h1>
+      </div>
+      <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+        <nav
+          className="text-muted-foreground grid gap-4 text-sm"
+          x-chunk="dashboard-04-chunk-0"
+        >
+          <Link
+            href="#"
+            className="text-primary font-semibold"
+          >
+            General
+          </Link>
+          <Link href="#">Security</Link>
+          <Link href="#">Integrations</Link>
+          <Link href="#">Support</Link>
+          <Link href="#">Organizations</Link>
+          <Link href="#">Advanced</Link>
+        </nav>
+        {children}
+      </div>
+    </main>
+  )
+}
+
+export default SettingsLayout
